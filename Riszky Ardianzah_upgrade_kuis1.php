169de02347d1bcb4428d523a74c9c9de247fcadc
riszkyardianzah <riszkyardianzah@gmail.com>
2016-11-25 17:46:43 +0700

    Upgrade Nilai Kuis Semester 3
    Dalam Upgrade Nilai Kuis ini terdapat 2 soal :
    1. UNK 1
    2. UNK 2

---
+++ Riszky Ardianzah_upgrade_kuis1.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Riszky Ardianzah</title>
+</head>
+<body>
+	<h2>UPGRADE KUIS</h2>
+	<table border='1'>
+		<tr>
+		<th rowspan="2">INPUT</th>
+		<th colspan="5">OUTPUT</th>
+		</tr>
+		</tr>
+			<th>Jumlah A-Z</th>
+		</tr>
+		<tr>
+		 	<td>
+			<?php
+			$artikel1 = "Menanggapi pemberitaan tentang Akademi Komunitas Negeri (AKN) di media masa belakangan ini, kami yang ditugasi  Bapak Bupati Bojonegoro sebagai Ketua Panitia pendirian PTN ( Akademi Komunitas Negeri) mencoba memberikan pencerahan terkait seputar pendirian AKN di Bojonegoro. Semua sudah sesuai dengan mekanisme yang ada dan perlu didukung oleh pihak terkait.
+
+Pendirian AKN itu amanat Undang-Undang (UU) Pendidikan Tinggi yang baru, yaitu UU No.12 Tahun 2012 yang telah disahkan DPRI RI tahun 2012. Harusnya daerah bangga karena diberi kesempatan untuk pendirian PTN setingkat D1 dan D2. Aturan itu diikuti Keputusan Mendikbud  No.161/P/2012 yaitu menugasi Politeknik Negeri Malang untuk membuka Prodi Diluar Domisili (PDD) di Bojonegoro dalam rangka pendirian AKN di Bojonegoro. Oleh karena itu, Yudi Pramono ditunjuk sebagai Ketua PDD di Bojonegoro (maaf bukan Direktur).
+
+Saat ini memang belum mandiri AKN seperti yang diberitakan, tetapi sifatnya Rintisan Akademi komunitas (Bukan Rintisan Polinema, karena Polinema sudah mandiri lama) itu hanya sementara, sampai dengan AKN itu mandiri. Pendirian AKN ini sudah diatur dalam Permendikbud  No. 48 Tahun 2013 yang sebelumnya berbentuk ketentuan dan persyaratan pendirian mulai tahun 2013 sudah dibakukan menjadi Kepmendikbud . 
+
+Bojonegoro adalah termasuk di Bact I  yang dipilih Dikti dengan total berjumlah 20 calon AKN di Indonesia dari 185 yang mengajukan pendirian AKN. Sedang Polinema ditugasi membina pada Bact I, yaitu Blitar, Jepara dan Taliwang, Sumbawa Barat. Setelah itu disusul Trenggalek, Lumajang dan Pamekasan. Sedangkan Lamongan dan Sumenep dibina oleh Pens Surabaya.  
+
+Saat ini Kabupaten Pacitan dan Blitar sudah mandiri, karena telah melengkapi persyaratannya. Target yang diharapkan menurut UU tersebut kedepan AKN ini ada untuk semua kota/ kabupaten di Indonesia dan sekarang baru sekitar 85 kota dan kabupaten yang sedang dirintis dan didanai Dikti. Untuk pembina yang ditunjuk Dikti baru sekitar 5 kota atau kabupaten yang sudah mandiri di Indonesia. Semoga saja Bojonegoro yang ke enamnya.
+
+Pendirian PTN berbeda dengan PTS. Kalau Negeri harus ada pendampingan atau pembina dari PTN yang ditunjuk Dikti. Contoh Pens Surabaya dulunya didampingi atau dibina ITS, Polinema didampingi UB Malang dan AKN Bojonegoro ditunjuk sesuai Permen diatas yaitu Polinema sebagai satker pembinanya sampai mandiri. Jadi, semua mengikuti ketentuan Polinema, termasuk anggaran pembiayaan DIPA nya. PDD Bojonegoro tidak mengelola keuangan, kecuali menyalurkan anggaran yang telah ditetapkan Polinema, semisal gaji, pihak Bojonegoro tinggal mengajukan SJ, Alat-alat praktek dan tender juga Polinema. Kita tinggal menerima alat, termasuk kegiatan-kegiatan kemahasiswaan dan lainya, semua mengajukan ke Polinema. 
+
+Memang ada SPP (UKP) tapi 100 persen harus disetor ke Polinema dan itu sangat ringan untuk standar kuliah di PTN dengan nominal hanya Rp900.000/semester atau 150.000/ bulan. Wisuda dan ijasah, semua juga dari Polinema.
+
+Kedepan, setelah mandiri semua pembiayaan bangunan, alat, gaji pegawai dan lain-lain akan ditanggung seluruhnya dari Dikti dan Kang Yoto, sapaan akrab Bupati Bojonegoro berharap AKN ini ke depan bisa menjadi Poltek yaitu membuka D3, D4 (S1) atau bahkan bisa saja dibuka S2 dan S3 Terapan. Karena sudah diatur di UU Pendidikan Tinggi No.12/2012.
+
+Terkait larangan kelas jauh, PDD ini berbeda dengan kelas jauh, karena Kepmendikbud No.161/P/2012 itu mengamanatkan kepada PTN pembina untk membuka Prodi Diluar Domisili di Kota atau Kabupaten yang akan didirikan AKN, jadi SK PDD tidak berlaku untuk PTN/PTS yang hanya buka kelas jauh tanpa ada rintisan PTN baru. Konsep PDD ini dibuat oleh Dikti dalam rangka proses pendirian PTN di daerah.
+
+Setelah terbit Kepmendikbud diatas, maka untuk menguji komitmen dan kelayakan, serta minat masyarakat, maka PDD Bojonegoro mulai 19 September Tahun 2012 diizinkan untuk menerima mahasiswa baru. Saat ini dari 85 Rintisan AKN di Indonesia, Bojonegoro yang terbesar dan menjadi percontohan di Indonesia dengan jumlah kisaran 1.000 mahasiswa. Dalam persyaratan pendirian disebutkan, bahwa Bupati sanggup untuk menyediakan tempat sementara, Tenaga Dosen dan staf sementara dan ditetapkan SMKN1 dan SMKN2 menjadi tempat sementara sesuai arahan Bupati Bojonegoro, Suyoto.
+
+Masalah sewa, mulai tahun 2012 dicoba diusulkan sewa ke Polinema, ternyata ditolak oleh Dikti dan BPK RI, karena sama-sama Aset Negara tidak boleh disewakan sebagai kompensasi Polinema mengizinkan seluruh peralatan dan perlengkapan lain untuk digunakan praktik siswa SMKN1 dan SMKN 2, termasuk bandwicth atau koneksi internet dengan Polinema membantu perawatan dan pemeliharaan alat. Jadi tidak benar kalau ada sewa diterima SMKN 1 dan SMKN 2.
+
+Terhadap PNS yang ditugasi jadi dosen sementara, memang diizinkan untuk proses pendirian ini. Setelah mandiri semua kembali ke Satkernya masing-masing dan AKN akan diisi oleh Dikti. Kita semua hanya mengantarkan untuk bisa menjadi PTN di Bojonegoro yang diamanatkan Kang Yoto.
+
+Untuk diketahui, bahwa rintisan pendirian AKN oleh Dikti saat itu yang mengajukan untuk bisa didirikan PTN berjumlah 185 kota/Kabupaten se indonesia hanya diambil 20 kota/kabupaten dan Bojonegoro yang terbaik dan terbesar sekaligus sebagai percontohan nasional. Jadi sangat disayangkan kalau program ini tidak didukung semua pihak, apalagi Bojonegoro dengan masih banyaknya lulusan SLTA sederajat yang tidak melanjutkan ke PT. Maka AKN di Bojonegoro sangat tepat untuk masyarakat Bojonegoro, juga letak Bojonegoro sangat strategis akan memberi efek mensejahterakan rakyat. Juga lokasi kampus di Dukuh Kedungrejo, Desa Ngumpakdalem, Kecamatan Dander dan sekitarnya yang sangat tandus untuk pertanian, maka dengan adanya kampus AKN ke depannya diharapkan bisa mengangkat harkat dan martabat masyarakat sekitar.
+
+Terkait masalah beasiswa, tidak benar kalau hanya PDD Bojonegoro yang menerima. Data sudah jelas banyak mahasiswa dari Kabupaten Bojonegoro yang menerima beasiswa dan kuliah di beberapa PTN dan PTS. Memang PDD jumlah mahasiswa penerimanya lebih banyak, sebagai strategi Bapak Bupati agar proses PTN di Bojonegoro bisa segera dimandirikan Dikti.
+
+Akhirnya, saya mengajak semua pihak untuk turut mendukung  dan memperlancar proses pendirian PTN di Kabupaten Bojonegoro. Kalau ada hal-hal yang kurang, dicari solusinya bersama yang baik dengan kepala dingin. Masalah hibah daerah ke pusat sudah jelas, tidak ada masalah. Contoh yang baru yaitu barusan minggu ini ITB di Jatinangor telah dihibahkan dari Pemprov Bandung ke Dikti, juga makam Bung Karno dihibahkan ke Pusat. Terima kasih, semoga semua pihak bisa menerima dengan legowo. Amiin.
+
+*Kepala SMKN 1 Bojonegoro sekaligus Ketua Panitia Pendirian AKN Bojonegoro (Ketua PDD Polinema di Bojonegoro)" ;
+			$artikel2 = strtoupper($artikel1);
+			echo $artikel1;
+			?>
+			</td>
+			<td>
+			<?php
+			$A = substr_count($artikel2,"A") ;
+			$B = substr_count($artikel2,"B") ;
+			$C = substr_count($artikel2,"C") ;
+			$D = substr_count($artikel2,"D") ;
+			$E = substr_count($artikel2,"E") ;
+			$F = substr_count($artikel2,"F") ;
+			$G = substr_count($artikel2,"G") ;
+			$H = substr_count($artikel2,"H") ;
+			$I = substr_count($artikel2,"I") ;
+			$J = substr_count($artikel2,"J") ;
+			$K = substr_count($artikel2,"K") ;
+			$L = substr_count($artikel2,"L") ;
+			$M = substr_count($artikel2,"M") ;
+			$N = substr_count($artikel2,"N") ;
+			$O = substr_count($artikel2,"O") ;
+			$P = substr_count($artikel2,"P") ;
+			$Q = substr_count($artikel2,"Q") ;
+			$R = substr_count($artikel2,"R") ;
+			$S = substr_count($artikel2,"S") ;			
+			$T = substr_count($artikel2,"T") ;
+			$U = substr_count($artikel2,"U") ;
+			$V = substr_count($artikel2,"V") ;
+			$W = substr_count($artikel2,"W") ;
+			$X = substr_count($artikel2,"X") ;
+			$Y = substr_count($artikel2,"Y") ;
+			$Z = substr_count($artikel2,"Z") ;
+
+			$total=$A+$B+$C+$D+$E+$F+$G+$H+$I+$J+$K+$L+$M+$N+$O+$P+$Q+$R+$S+$T+$U+$V+$W+$X+$Y+$Z;
+			echo $total;
+			?>
+			</td>
+		</tr>
+	</table>
+
+</body>
+</html>
